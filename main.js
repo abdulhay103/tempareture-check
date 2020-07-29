d3050cbc4535d2833ba6a52d60d99271a9c4f0e1
abdulhay103 <mdabdulhay95@gmail.com>
2020-07-29 19:14:28 +0600

    Get Value Problem solve

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-let catchId = document.getElementById("catch");
-let submitData = document.getElementById("collectTem").value;
+const catchId = document.getElementById("catch");
+const catchSubmitDataId = document.getElementById("collectTem");
+let customData = catchSubmitDataId.value;
 
 function toCelcious() {
-  console.log(submitData);
-  let degCelcious = (5 / 9) * (submitData - 32);
-  let currentTempareture = Math.floor(degCelcious);
+  console.log(customData);
+  let degCelcious = (5 / 9) * (customData - 32);
+  let currentTempareture = Math.round(degCelcious);
   catchId.innerHTML = "Your Tempareture is " + currentTempareture + " &#8451";
 }
